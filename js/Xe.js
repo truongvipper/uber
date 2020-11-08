7492ae3fb74cd74fd75612790ac5d810e8d42701
unknown <baoduong101110@gmail.com>
2020-11-08 16:17:52 +0700

    themXe

---
+++ js/Xe.js
@@ -0,0 +1,8 @@
+var Xe = function(){
+    this.bienSo='';
+    this.loaiXe='';
+    this.xuatThongTin() = function(){
+        console.log(`Biển số:',${this.bienSo}`);
+        console.log(`Loại xe:',${this.loaiXe}`);
+    }
+}
